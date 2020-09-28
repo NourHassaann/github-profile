920dd2154d734e02eb2b36345a9c80da1b41c694
NourHassaann <nurhanhassaan@gmail.com>
2020-09-28 16:34:21 +0200

    first commit

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const APIURL = "http://api.github.com/users";
 const main = document.getElementById('main'); 
 const form = document.getElementById('form'); 
-const search = document.getElementById('search'); 
+const search = document.getElementById('search');     
 
 async function getUser (user){
     const resp = await fetch(`${APIURL}/${user}`);
@@ -11,15 +11,15 @@ async function getUser (user){
 function createUserCard(user){
     const cardHTML = `
     <div class="max-w-sm mt-4 rounded overflow-hidden shadow-lg">
-        <img class="w-full" src=${user.avatar_url} alt=${user.name}>
-            <div class="px-6 py-4">
-            <div class="font-bold text-xl mb-2">${user.name ? user.name : "this user has no name"}</div>
-            <p class="text-gray-700 text-base">${user.bio}</p>
+        <img class="w-full bg-white-200" src=${user.avatar_url} alt=${user.name}>
+            <div class="px-6 py-4 bg-purple-200">
+            <div class="font-bold text-xl mb-2 ">${user.name ? user.name : "This user has no name"}</div>
+            <p class="text-gray-700 text-base">${user.bio ? user.bio : "No bio yet !"}</p>
             </div>
-            <div class="px-6 pt-4 pb-2">
-                <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">followers: ${user.followers}</span>
-                <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">${user.following}</span>
-                <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">${user.public_repos}</span>
+            <div class="px-6 pt-4 pb-2 bg-orange-200">
+                <span class="inline-block rounded-full px-3 py-1 text-sm font-semibold text-gray-800 mr-2 mb-2">Followers: ${user.followers}</span>
+                <span class="inline-block rounded-full px-3 py-1 text-sm font-semibold text-gray-800 mr-2 mb-2">Following: ${user.following}</span>
+                <span class="inline-block rounded-full px-3 py-1 text-sm font-semibold text-gray-800 mr-2 mb-2">Public_repos: ${user.public_repos}</span>
             </div>
     </div>
     `
